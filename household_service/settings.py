2953c75e78365d476135a90dcd8a77fa900aa041
shufolislam1 <shufolislam1255@gmail.com>
2024-03-12 22:08:51 +0600

    restrructured

--- household_service/settings.py
+++ household_service/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'household_service.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates',],
+        'DIRS': ['templates' ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,11 +124,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'assets/'
+STATIC_URL = 'static/'
 AUTH_USER_MODEL = 'core.User'
-STATICFILES_DIRS = [
-    BASE_DIR / "assets",
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'assets'),
+# ]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
