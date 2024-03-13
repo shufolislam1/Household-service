1d8cde048c2a0736f131ef9ad7281807884c75ca
shufolislam1 <shufolislam1255@gmail.com>
2024-03-13 23:50:05 +0600

    add to cart donee

--- cart/urls.py
+++ cart/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('add_to_cart/<int:id>/', views.add_to_cart, name='add_to_cart'),
+    path('show_cart', views.show_cart, name='show_cart')
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,14 +1,29 @@
 from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
 from services.models import Services
 from .models import Cart, CartItems
+from django.contrib import messages
+from django.urls import reverse
 from django.http import HttpResponseRedirect
 # Create your views here.
+
 
 def add_to_cart(request, id):
     service = Services.objects.get(id = id)
     user = request.user
-    cart= Cart.objects.get_or_create(user = user, is_paid = False)
+    
+    if not user.is_authenticated:
+        messages.warning(request, "You need to log in to add items to your cart.")
+        return redirect(reverse('userLogin'))
+    
+    cart, _ = Cart.objects.get_or_create(user = user, is_paid = False)
     cart_item = CartItems.objects.create(cart = cart, service = service)
     cart_item.save()
-    return redirect('home')
+    messages.success(request, "Item added to cart successfully.")
+    return redirect('show_cart')
+
+def show_cart(request):
+    user = request.user
+    cart_items = CartItems.objects.filter(cart__user=user, cart__is_paid=False)
+    return render(request, 'show_cart.html', {'cart_items': cart_items})
 
